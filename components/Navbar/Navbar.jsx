0fd1e9e0048b1cc88bd9d67818f7715032c4270a
Anvarsha C <153602698+anvarshac@users.noreply.github.com>
2024-08-10 16:19:56 +0530

    Add files via upload

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,186 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='body'>
+      <div className='navbar'>
+        <img src="https://th.bing.com/th/id/R.6e612a439cad2a6c4af5d13c2b6c72f4?rik=LM%2fJQ%2fI%2fMm3a5A&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_140761.png&ehk=uCeTcA%2bEP90%2ffh82hPzvTlKgjzuxkkdWiR%2f0LhlPGmU%3d&risl=&pid=ImgRaw&r=0" alt="" className='logo' />
+
+        <h1 className='academy'>Ernest Academy</h1>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Features</li>
+          <li>About</li>
+        </ul>
+
+        <div className='search-box'>
+          <input type="text" placeholder='Search' />
+
+
+        </div>
+
+
+
+      </div>
+
+      <div className='main'>
+        <div className='main-text'>
+          <h4>Personal Education.</h4>
+          <h2>Lifetime Success</h2>
+          <p className='para-main'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores possimus in nisi incidunt, exercitationem pariatur! Doloremque dolores architecto laborum laudantium qui dicta debitis? Blanditiis quo harum maxime laboriosam accusantium eveniet.</p>
+          <button className='button'>Learn More</button>
+
+          <div className='login'>
+
+          </div>
+
+          <div className='courses-part'>
+            <h1 className='course-header'>OUR POPULAR COURSES</h1>
+            <div className='box-part'>
+              <div className='business-box'>
+                <img className='bag' src="https://th.bing.com/th/id/OIP.9_N9-GpID-EBtG7nhr-MmAHaHa?rs=1&pid=ImgDetMain" alt="" />
+                <h4 className='business'>Business</h4>
+                <p className='business-para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, ad optio. Magni dolores, pariatur delectus earum tempore ratione recusandae sunt reprehenderit. Consectetur sit cumque atque earum maxime animi nesciunt quae?</p>
+                <button className='join-but'>Join Now</button>
+              </div>
+              <div className='maths-box'>
+                <img className='maths-icon' src="https://thumbs.dreamstime.com/b/black-silhouette-drawing-compass-pencil-set-vector-illustration-79733921.jpg" alt="" />
+                <h4 className='maths'>Maths</h4>
+                <p className='maths-para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, ad optio. Magni dolores, pariatur delectus earum tempore ratione recusandae sunt reprehenderit. Consectetur sit cumque atque earum maxime animi nesciunt quae?</p>
+                <button className='join-but'>Join Now</button>
+              </div>
+
+              <div className='science-box'>
+                <img className='science-icon' src="https://static.vecteezy.com/system/resources/previews/010/918/269/original/experiment-line-icon-free-vector.jpg" alt="" />
+                <h4 className='science'>Science</h4>
+                <p className='science-para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, ad optio. Magni dolores, pariatur delectus earum tempore ratione recusandae sunt reprehenderit. Consectetur sit cumque atque earum maxime animi nesciunt quae?</p>
+                <button className='join-but'>Join Now</button>
+              </div>
+
+              <div className='tech-box'>
+                <img className='tech-icon' src="https://static.vecteezy.com/system/resources/previews/000/599/770/original/technology-logo-icon-vectors.jpg" alt="" />
+                <h4 className='tech'>Technology</h4>
+                <p className='tech-para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, ad optio. Magni dolores, pariatur delectus earum tempore ratione recusandae sunt reprehenderit. Consectetur sit cumque atque earum maxime animi nesciunt quae?</p>
+                <button className='join-but'>Join Now</button>
+              </div>
+              <div className='eco-box'>
+                <img className='eco-icon' src="https://static.vecteezy.com/system/resources/previews/009/463/331/original/calculator-black-and-white-icon-design-element-on-isolated-white-background-free-vector.jpg" alt="" />
+                <h4 className='eco'>Economics</h4>
+                <p className='eco-para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, ad optio. Magni dolores, pariatur delectus earum tempore ratione recusandae sunt reprehenderit. Consectetur sit cumque atque earum maxime animi nesciunt quae?</p>
+                <button className='join-but'>Join Now</button>
+              </div>
+            </div>
+          </div>
+
+          <div className='build'>
+
+            <img src="https://cdn.britannica.com/06/145006-050-78D8EE94/Samuel-Phillips-Hall-Mass-Academy-Andover.jpg" alt="" className='build-img' />
+            <p className='build-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ex incidunt ea reiciendis doloribus dolorem voluptate vero. Libero quisquam dolore eveniet consequatur, nam, excepturi officia accusantium dolor odio voluptatibus sapiente Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsum natus minima similique possimus fuga illo laboriosam laudantium vel consequatur esse voluptate tempore saepe quam, dignissimos alias accusamus enim voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. In consequuntur consequatur vero soluta ratione tempore, necessitatibus sint, amet corrupti animi eaque optio dolorum nesciunt, voluptatum excepturi officiis perferendis ut ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ea cum porro laborum suscipit adipisci dolore iste. Earum molestias incidunt eligendi. Aliquid laboriosam, nostrum quas commodi asperiores veritatis omnis velit.</p>
+
+          </div>
+          <div className='build-inner'>
+
+
+            <img src="https://www.wheatleyfm.com/wp-content/uploads/2013/02/lwa-004-2.jpg" alt="" className='build-inner-img' />
+            <p className="build-inner-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus minima quaerat voluptatum id voluptate quas, explicabo facilis dicta fugit unde placeat inventore at ipsa et iure magnam, veniam labore dolorem? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, architecto dolores. Tenetur sunt sint et magni officia. Quis laudantium itaque aspernatur architecto fuga doloremque dolores qui magnam possimus voluptas? Numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis distinctio iste molestias optio iure. Quae similique sunt aperiam modi ea tenetur fugiat reiciendis a illo deserunt labore quisquam, optio blanditiis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nobis, odio, porro ea vitae dolores dicta non, maxime quisquam asperiores cumque commodi debitis quasi aliquam? Fuga atque vero quia quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est repellendus, inventore autem veniam exercitationem nostrum fugit nam labore voluptatum! Ducimus, quas consequuntur? Veniam tempore debitis provident possimus error, ipsum maiores!
+
+            </p>
+          </div>
+
+          <div className='build-class'>
+            <img src="https://fcfreepresspa.com/wp-content/uploads/2021/01/schools.jpg" alt="" className='build-class-img' />
+
+
+            <p className='build-class-p'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet corrupti voluptatibus quisquam perspiciatis quae molestiae, dolore neque illum obcaecati voluptate voluptatum quas error architecto nulla? Rem in voluptatem dolorum impedit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa veritatis pariatur dolor excepturi consequatur distinctio eum expedita nisi est maiores accusamus assumenda vitae dolorem, quaerat ut voluptas temporibus ab obcaecati.</p>
+
+          </div>
+
+          <div className='welcome'>
+            <img src="https://www.usnews.com/cmsmedia/a1/7c/61548aea4db9ab9045673bb5187f/160919-studentscollege-stock.jpg" alt="" className='welcome-img'/>
+            <h1 className='welcome-h'>We Welcome All...</h1>
+            <p className='welcome-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, in impedit ipsum est, autem nemo praesentium molestias dolorem facilis corrupti neque magni sunt commodi consequuntur nulla, iusto repellat natus aliquam.</p>
+
+          </div>
+        </div>
+
+        <div className="ideas">
+          <h1 className='ideas-h'>WE ADVANCE IDEAS AND HUMANITY</h1>
+        </div>
+
+        <div className='ideas-boxpart'>
+          <div className='speechpart'>
+            <img src="https://thumbs.dreamstime.com/b/pretty-young-business-woman-giving-presentation-conference-meeting-setting-shallow-dof-color-toned-image-154884743.jpg" alt="" className='speech-img' />
+
+            <h1 className='speech-h'>Intellectual freedom</h1>
+            <p className='speech-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nisi sapiente, voluptatum adipisci, obcaecati ipsum soluta quis, ex autem incidunt labore quia neque quidem. Harum libero sed eos itaque asperiores.</p>
+          </div>
+
+          <div className='interactpart'>
+
+            <img src="https://letstalkscience.ca/sites/default/files/2024-02/96.png" alt="" className='interact-img' />
+            <h1 className='interact-h' >Community impact</h1>
+            <p className='interact-p'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat incidunt harum laboriosam voluptatibus cum, necessitatibus recusandae explicabo. Ipsum tempora corporis ratione veniam recusandae dolorum exercitationem maxime magnam, harum, consectetur porro!</p>
+
+          </div>
+
+          <div className='naturepart'>
+
+                    <img src="https://funcabinrentals.com/wp-content/uploads/2018/04/Things-to-do-in-yosemite-national-park-768x511.jpeg" alt="" className='nature-img' />
+                    <h1 className='nature-h'>Global impact</h1>
+                    <p className='nature-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, nisi omnis vitae itaque excepturi aliquam eum perferendis dolores inventore magnam sequi provident exercitationem, suscipit voluptates veritatis nemo! Laboriosam, beatae cum.</p>
+
+          </div>
+
+
+        </div>
+
+        <div className='footer'>
+          <div className='f-h'>
+            <h1 className='loc' >
+              LOCATION
+
+            </h1>
+            <h1 className='ph'>PHONE</h1>
+            <h1 className='fu'>FOLLOW US</h1>
+
+          </div>
+
+          <div className='fh-con'>
+            <div className='fh-loc-con'>
+              <p>2005 Sooke Road</p>
+
+              <p>Victoria, BC  V9B 5Y2</p>
+
+              <p>Canada</p>
+            </div>
+            <div className='fh-ph-con'>
+              <p>250.391.2511</p>
+              <p>Toll-free: 1.800.788.8028</p>
+
+            </div>
+            <div className='fh-fu-con'>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Facebook_icon_(black).svg/1024px-Facebook_icon_(black).svg.png?20160411105641" alt="" className='fb' />
+
+              <img src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Emblem.png" alt="" className='insta' />
+
+              <img src="https://th.bing.com/th/id/R.42f8397a5e20e9f401d149a24b8c1bf3?rik=tuidqrxOnL4itw&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2011%2f01%2flinkedin-logo-black_175153.png&ehk=fz3fSPnv0eQwacxlfr5I3pZ%2faOf%2bxG9cG11Hq83KpAA%3d&risl=&pid=ImgRaw&r=0" alt="" className='in' />
+
+
+
+
+
+            </div>
+
+          </div>
+
+          <p className='fh-academy'>©2024 Ernest Academy of Chicago</p>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
